baf4aab3eb0d16346541e3295505a1ad89c648d6
kameyama <kameyama@kameyama-no-MacBook-Pro.local>
2018-09-08 14:52:56 +0900

    add add-data and edit-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SQLite } from '@ionic-native/sqlite';
+import { Toast } from '@ionic-native/toast';
+import { AddDataPage } from '../pages/add-data/add-data';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddDataPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +23,15 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddDataPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    SQLite,
+    Toast
   ]
 })
-export class AppModule {}
+export class AppModule { }
